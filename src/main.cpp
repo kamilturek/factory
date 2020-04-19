35eb1954cd9a47b81c6a3a6010c44ca85b5791dc
Kamil Turek <tazzu@live.com>
2020-04-19 23:47:53 +0200

    Initialize classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
-#include <functional>
-#include <iostream>
+#include "Car.hpp"
+#include "Factory.hpp"
+
+void run()
+{
+    Factory factory;
+}
 
 int main(int /*argc*/, char const** /*argv*/)
 {
+    run();
+
     return 0;
 }
